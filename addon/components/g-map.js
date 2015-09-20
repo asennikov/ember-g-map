dbc45bc8a36482eeda4a5b804ea0ea6fe7c450cd
Alexander Sennikov <gseno88@gmail.com>
2015-09-21 00:06:47 +0300

    Add g-map component showing centered and zoomed map

--- addon/components/g-map.js
+++ addon/components/g-map.js
@@ -1,6 +1,46 @@
 import Ember from 'ember';
 import layout from '../templates/components/g-map';
 
+const { isEmpty, isPresent, observer, run } = Ember;
+
 export default Ember.Component.extend({
-  layout: layout
+  layout: layout,
+  classNames: ['g-map'],
+
+  didInsertElement() {
+    this._super();
+    if (isEmpty(this.get('map'))) {
+      let canvas = this.$().find('.g-map-canvas').get(0);
+      this.set('map', new google.maps.Map(canvas, null));
+    }
+    this.setZoom();
+    this.setCenter();
+  },
+
+  zoomChanged: observer('zoom', function() {
+    run.once(this, 'setZoom');
+  }),
+
+  setZoom() {
+    let map = this.get('map');
+    let zoom = this.get('zoom');
+    if (isPresent(map)) {
+      map.setZoom(zoom);
+    }
+  },
+
+  coordsChanged: observer('lat', 'lng', function() {
+    run.once(this, 'setCenter');
+  }),
+
+  setCenter() {
+    let map = this.get('map');
+    let lat = this.get('lat');
+    let lng = this.get('lng');
+
+    if (isPresent(map) && isPresent(lat) && isPresent(lng)) {
+      let center = new google.maps.LatLng(lat, lng);
+      map.setCenter(center);
+    }
+  }
 });
---
+++ tests/unit/components/g-map-test.js
@@ -0,0 +1,120 @@
+import Ember from 'ember';
+import { moduleForComponent, test } from 'ember-qunit';
+import sinon from 'sinon';
+
+const { run } = Ember;
+
+const fakeMapObject = {
+  setCenter: sinon.stub(),
+  setZoom: sinon.stub()
+};
+
+moduleForComponent('g-map', 'Unit | Component | g map', {
+  // Specify the other units that are required for this test
+  // needs: ['component:foo', 'helper:bar'],
+  unit: true,
+  beforeEach() {
+    sinon.stub(google.maps, 'Map').returns(fakeMapObject);
+  },
+  afterEach() {
+    google.maps.Map.restore();
+  }
+});
+
+test('it should construct new `Map` object after render', function(assert) {
+  let component = this.subject();
+
+  this.render();
+  assert.ok(google.maps.Map.calledOnce);
+  assert.equal(component.get('map'), fakeMapObject);
+});
+
+test('new `Map` shouldn\'t constructed if it already present in component', function(assert) {
+  this.subject({
+    map: fakeMapObject
+  });
+  this.render();
+
+  assert.equal(google.maps.Map.callCount, 0);
+});
+
+test('it should trigger `setZoom` on `didInsertElement` event', function(assert) {
+  let component = this.subject();
+  this.render();
+
+  component.setZoom = sinon.spy();
+  component.trigger('didInsertElement');
+  assert.ok(component.setZoom.calledOnce);
+});
+
+test('it should trigger `setCenter` on `didInsertElement` event', function(assert) {
+  let component = this.subject();
+  this.render();
+
+  component.setCenter = sinon.spy();
+  component.trigger('didInsertElement');
+  assert.ok(component.setCenter.calledOnce);
+});
+
+test('it should trigger `setZoom` on `zoom` change', function(assert) {
+  let component = this.subject();
+  this.render();
+
+  component.setZoom = sinon.spy();
+  run(() => component.set('zoom', 14));
+  assert.ok(component.setZoom.calledOnce);
+});
+
+test('it should call `setCenter` of google map on `setCenter` with lat/lng present', function(assert) {
+  let component = this.subject({
+    lat: 10,
+    lng: 100
+  });
+  this.render();
+
+  fakeMapObject.setCenter = sinon.stub();
+  run(() => component.setCenter());
+  assert.ok(fakeMapObject.setCenter.calledOnce);
+});
+
+test('it should call `setZoom` of google map on `setZoom` with zoom present', function(assert) {
+  let component = this.subject({
+    zoom: 14
+  });
+  this.render();
+
+  fakeMapObject.setZoom = sinon.stub();
+  run(() => component.setZoom());
+  assert.ok(fakeMapObject.setZoom.calledOnce);
+});
+
+test('it should not call `setCenter` of google map on `setCenter` when no lat present', function(assert) {
+  let component = this.subject({
+    lat: 10
+  });
+  this.render();
+
+  fakeMapObject.setCenter = sinon.stub();
+  run(() => component.setCenter());
+  assert.equal(fakeMapObject.setCenter.callCount, 0);
+});
+
+test('it should not call `setCenter` of google map on `setCenter` when no lng present', function(assert) {
+  let component = this.subject({
+    lng: 10
+  });
+  this.render();
+
+  fakeMapObject.setCenter = sinon.stub();
+  run(() => component.setCenter());
+  assert.equal(fakeMapObject.setCenter.callCount, 0);
+});
+
+test('it should not call `setZoom` of google map on `setZoom` when no zoom present', function(assert) {
+  let component = this.subject();
+  this.render();
+
+  fakeMapObject.setZoom = sinon.stub();
+  run(() => component.setZoom());
+  assert.ok(fakeMapObject.setZoom.calledOnce);
+});
