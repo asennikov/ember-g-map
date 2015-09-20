a5ff474542c4bb1a837e2b744fe50a934506dad6
Alexander Sennikov <gseno88@gmail.com>
2015-09-20 16:00:01 +0300

    Load google-map lib on page load

--- index.js
+++ index.js
@@ -2,5 +2,30 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-g-map'
+  name: 'ember-g-map',
+
+  contentFor: function(type, config) {
+    var content = '';
+
+    if (type === 'head') {
+      var src = "//maps.googleapis.com/maps/api/js";
+      var gMapConfig = config['g-map'] || {};
+      var params = [];
+
+      var key = gMapConfig.key;
+      if (key) {
+        params.push('key=' + encodeURIComponent(key));
+      }
+
+      var libraries = gMapConfig.libraries;
+      if (libraries && libraries.length) {
+        params.push('libraries=' + encodeURIComponent(libraries.join(',')));
+      }
+
+      src += '?' + params.join('&');
+      content = '<script type="text/javascript" src="' + src + '"></script>';
+    }
+
+    return content;
+  }
 };
