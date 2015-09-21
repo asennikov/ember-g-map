916ef06a0d96ef860d7a875ac8d4f89c80b65c21
Alexander Sennikov <gseno88@gmail.com>
2015-09-21 23:48:20 +0300

    Refactor marker tests

--- tests/unit/components/g-map-marker-test.js
+++ tests/unit/components/g-map-marker-test.js
@@ -32,208 +32,203 @@ moduleForComponent('g-map-marker', 'Unit | Component | g map marker', {
 });
 
 test('it constructs new `Marker` object after render', function(assert) {
-  this.render();
-
+  component.trigger('didInsertElement');
   sinon.assert.calledOnce(google.maps.Marker);
   assert.equal(component.get('marker'), fakeMarkerObject);
 });
 
 test('new `Marker` isn\'t constructed if it already present in component', function() {
   run(() => component.set('marker', fakeMarkerObject));
-  this.render();
-
+  component.trigger('didInsertElement');
   sinon.assert.notCalled(google.maps.Marker);
 });
 
 test('it triggers `setMap` on `didInsertElement` event', function() {
-  this.render();
-
   component.setMap = sinon.spy();
   component.trigger('didInsertElement');
   sinon.assert.calledOnce(component.setMap);
 });
 
 test('it triggers `setMap` of marker with null on `willDestroyElement` event if marker is set', function() {
-  run(() => component.set('marker', fakeMarkerObject));
-  this.render();
-
   fakeMarkerObject.setMap = sinon.spy();
+
+  run(() => component.set('marker', fakeMarkerObject));
   component.trigger('willDestroyElement');
+
   sinon.assert.calledOnce(fakeMarkerObject.setMap);
   sinon.assert.calledWith(fakeMarkerObject.setMap, null);
 });
 
 test('it doesn\'t trigger `setMap` of marker on `willDestroyElement` event if there is no marker', function() {
-  this.render();
+  fakeMarkerObject.setMap = sinon.spy();
 
   run(() => component.set('marker', undefined));
-  fakeMarkerObject.setMap = sinon.spy();
   component.trigger('willDestroyElement');
+
   sinon.assert.notCalled(fakeMarkerObject.setMap);
 });
 
 test('it triggers `setMap` on `parentView.map` change', function() {
   run(() => component.set('parentView', { map: '' }));
-  this.render();
-
   component.setMap = sinon.spy();
   run(() => component.set('parentView.map', {}));
   sinon.assert.calledOnce(component.setMap);
 });
 
 test('it triggers `setPosition` on `didInsertElement` event', function() {
-  this.render();
-
   component.setPosition = sinon.spy();
   component.trigger('didInsertElement');
   sinon.assert.calledOnce(component.setPosition);
 });
 
 test('it triggers `setPosition` on `lat` change', function() {
-  this.render();
-
   component.setPosition = sinon.spy();
   run(() => component.set('lat', 14));
   sinon.assert.calledOnce(component.setPosition);
 });
 
 test('it triggers `setPosition` on `lng` change', function() {
-  this.render();
-
   component.setPosition = sinon.spy();
   run(() => component.set('lng', 21));
   sinon.assert.calledOnce(component.setPosition);
 });
 
 test('it triggers `setPosition` only once on `lat` and `lng` change', function() {
-  this.render();
-
   component.setPosition = sinon.spy();
-  run(() => component.setProperties({
-    lng: 1,
-    lat: 11
-  }));
+  run(() => component.setProperties({ lng: 1, lat: 11 }));
   sinon.assert.calledOnce(component.setPosition);
 });
 
 test('it calls `setPosition` of google marker on `setPosition` with lat/lng present', function() {
-  this.render();
+  let point = {};
+  sinon.stub(google.maps, 'LatLng').returns(point);
 
   run(() => component.setProperties({
     lat: 10,
-    lng: 100
+    lng: 100,
+    marker: fakeMarkerObject
   }));
 
   fakeMarkerObject.setPosition = sinon.stub();
-  let point = {};
-  sinon.stub(google.maps, 'LatLng').returns(point);
+
   run(() => component.setPosition());
+
   sinon.assert.calledOnce(fakeMarkerObject.setPosition);
   sinon.assert.calledWith(fakeMarkerObject.setPosition, point);
+
   google.maps.LatLng.restore();
 });
 
 test('it doesn\'t call `setPosition` of google marker on `setPosition` when no lat present', function() {
-  this.render();
-
-  run(() => component.set('lat', 10));
-
   fakeMarkerObject.setPosition = sinon.stub();
+
+  run(() => component.setProperties({
+    lng: 100,
+    marker: fakeMarkerObject
+  }));
   run(() => component.setPosition());
   sinon.assert.notCalled(fakeMarkerObject.setPosition);
 });
 
 test('it doesn\'t call `setPosition` of google marker on `setPosition` when no lng present', function() {
-  this.render();
-
-  run(() => component.set('lng', 10));
-
   fakeMarkerObject.setPosition = sinon.stub();
+
+  run(() => component.setProperties({
+    lat: 10,
+    marker: fakeMarkerObject
+  }));
   run(() => component.setPosition());
+
   sinon.assert.notCalled(fakeMarkerObject.setPosition);
 });
 
 test('it calls `setMap` of google marker on `setMap` with `map` present', function() {
-  this.render();
-
   let mapObject = {};
-  run(() => component.set('map', mapObject));
+  run(() => component.setProperties({
+    map: mapObject,
+    marker: fakeMarkerObject
+  }));
 
   fakeMarkerObject.setMap = sinon.stub();
+
   run(() => component.setMap());
+
   sinon.assert.calledOnce(fakeMarkerObject.setMap);
   sinon.assert.calledWith(fakeMarkerObject.setMap, mapObject);
 });
 
 test('it doesn\'t call `setMap` of google marker on `setMap` when no `map` present', function() {
-  this.render();
-
   fakeMarkerObject.setMap = sinon.stub();
   run(() => component.setMap());
   sinon.assert.notCalled(fakeMarkerObject.setMap);
 });
 
 test('it triggers `setIcon` on `didInsertElement` event', function() {
-  this.render();
-
   component.setIcon = sinon.spy();
   component.trigger('didInsertElement');
   sinon.assert.calledOnce(component.setIcon);
 });
 
 test('it triggers `setIcon` on `icon` change', function() {
-  this.render();
-
   component.setIcon = sinon.spy();
   run(() => component.set('icon', 'image-src'));
   sinon.assert.calledOnce(component.setIcon);
 });
 
 test('it calls `setIcon` of google marker on `setIcon` with icon present', function() {
-  run(() => component.set('icon', 'image-src'));
-  this.render();
+  run(() => component.setProperties({
+    icon: 'image-src',
+    marker: fakeMarkerObject
+  }));
 
   fakeMarkerObject.setIcon = sinon.stub();
+
   run(() => component.setIcon());
+
   sinon.assert.calledOnce(fakeMarkerObject.setIcon);
   sinon.assert.calledWith(fakeMarkerObject.setIcon, 'image-src');
 });
 
 test('it doesn\'t call `setIcon` of google marker on `setIcon` when no icon present', function() {
-  run(() => component.set('icon', undefined));
-  this.render();
-
   fakeMarkerObject.setIcon = sinon.stub();
+
+  run(() => component.setProperties({
+    icon: undefined,
+    marker: fakeMarkerObject
+  }));
   run(() => component.setIcon());
+
   sinon.assert.notCalled(fakeMarkerObject.setIcon);
 });
 
 test('it calls `setInfowindow` on `setMap` when `withInfowindow` is true', function() {
-  this.render();
+  component.setInfowindow = sinon.stub();
 
   run(() => component.set('map', {}));
-  run(() => component.set('withInfowindow', true));
-
-  component.setInfowindow = sinon.stub();
+  run(() => component.setProperties({
+    withInfowindow: true,
+    marker: fakeMarkerObject
+  }));
   run(() => component.setMap());
+
   sinon.assert.calledOnce(component.setInfowindow);
 });
 
 test('it doesn\'t call `setInfowindow` on `setMap` when `withInfowindow` is not true', function() {
-  this.render();
+  component.setInfowindow = sinon.stub();
 
   run(() => component.set('map', {}));
   run(() => component.set('withInfowindow', undefined));
-
-  component.setInfowindow = sinon.stub();
   run(() => component.setMap());
+
   sinon.assert.notCalled(component.setInfowindow);
 });
 
 test('it constructs new `Infowindow` on `setInfowindow` with `map` set', function() {
-  this.render();
-
-  run(() => component.set('map', {}));
+  run(() => component.setProperties({
+    map: {},
+    marker: fakeMarkerObject
+  }));
 
   let infowindow = {
     open: sinon.stub()
@@ -251,11 +246,9 @@ test('it constructs new `Infowindow` on `setInfowindow` with `map` set', functio
 });
 
 test('new `Infowindow` isn\'t constructed if no map is set', function() {
-  this.render();
+  sinon.stub(google.maps, 'InfoWindow');
 
   run(() => component.set('map', undefined));
-
-  sinon.stub(google.maps, 'InfoWindow');
   run(() => component.setInfowindow());
 
   sinon.assert.notCalled(google.maps.InfoWindow);
@@ -264,11 +257,9 @@ test('new `Infowindow` isn\'t constructed if no map is set', function() {
 });
 
 test('new `Infowindow` isn\'t constructed if no marker is set', function() {
-  this.render();
+  sinon.stub(google.maps, 'InfoWindow');
 
   run(() => component.set('marker', undefined));
-
-  sinon.stub(google.maps, 'InfoWindow');
   run(() => component.setInfowindow());
 
   sinon.assert.notCalled(google.maps.InfoWindow);
