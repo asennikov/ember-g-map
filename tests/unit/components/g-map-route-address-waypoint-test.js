49e5eeaaee164c6c73874b00b9bb0aece84d9af7
Alexander Sennikov <gseno88@gmail.com>
2016-04-24 15:00:16 +0300

    Add tests for route waypoints

---
+++ tests/unit/components/g-map-route-address-waypoint-test.js
@@ -0,0 +1,131 @@
+import Ember from 'ember';
+import { moduleForComponent } from 'ember-qunit';
+import test from '../../ember-sinon-qunit/test';
+import GMapComponent from 'ember-g-map/components/g-map';
+import GMapRouteComponent from 'ember-g-map/components/g-map-route';
+import sinon from 'sinon';
+
+const { run } = Ember;
+
+let fakePlacesService;
+let component;
+
+moduleForComponent('g-map-route-address-waypoint', 'Unit | Component | g map route address waypoint', {
+  // Specify the other units that are required for this test
+  // needs: ['component:foo', 'helper:bar'],
+  unit: true,
+
+  beforeEach() {
+    fakePlacesService = {
+      textSearch: sinon.stub()
+    };
+    sinon.stub(google.maps.places, 'PlacesService').returns(fakePlacesService);
+
+    const mapComponent = GMapComponent.create();
+    component = this.subject({
+      routeContext: GMapRouteComponent.create({ mapContext: mapComponent })
+    });
+  },
+
+  afterEach() {
+    google.maps.places.PlacesService.restore();
+  }
+});
+
+test('it calls `initPlacesService` on `didInsertElement`', function() {
+  component.initPlacesService = sinon.stub();
+  component.trigger('didInsertElement');
+  sinon.assert.calledOnce(component.initPlacesService);
+});
+
+test('it constructs new `PlacesService` on `initPlacesService` call', function(assert) {
+  run(() => component.set('map', {}));
+  run(() => component.initPlacesService());
+
+  sinon.assert.calledOnce(google.maps.places.PlacesService);
+  assert.equal(component.get('placesService'), fakePlacesService);
+});
+
+test('new `PlacesService` isn\'t being constructed if it\'s already present in component', function() {
+  run(() => component.setProperties({
+    placesService: fakePlacesService,
+    map: {}
+  }));
+  run(() => component.initPlacesService());
+
+  sinon.assert.notCalled(google.maps.places.PlacesService);
+});
+
+test('it triggers `initPlacesService` on `routeContext.map` change', function() {
+  run(() => component.set('routeContext', { map: '' }));
+  component.initPlacesService = sinon.spy();
+  run(() => component.set('routeContext.map', {}));
+  sinon.assert.calledOnce(component.initPlacesService);
+});
+
+test('it triggers `searchLocation` on `initPlacesService` call', function() {
+  component.searchLocation = sinon.stub();
+
+  run(() => component.set('map', {}));
+  run(() => component.initPlacesService());
+
+  sinon.assert.calledOnce(component.searchLocation);
+});
+
+test('it triggers `searchLocation` on `address` change', function() {
+  component.searchLocation = sinon.stub();
+  run(() => component.set('address', 'query string'));
+  sinon.assert.calledOnce(component.searchLocation);
+});
+
+test('it calls `textSearch` of placesService on `searchLocation`', function() {
+  run(() => component.set('address', 'query string'));
+  run(() => component.set('placesService', fakePlacesService));
+
+  fakePlacesService.textSearch = sinon.stub();
+  run(() => component.searchLocation());
+
+  const correctRequest = { query: 'query string' };
+
+  sinon.assert.calledOnce(fakePlacesService.textSearch);
+  sinon.assert.calledWith(fakePlacesService.textSearch, correctRequest);
+});
+
+test('it calls `updateLocation` after successful textSearch on `searchLocation`', function() {
+  const results = [{ a: 1 }, { b: 2 }];
+  const status = google.maps.places.PlacesServiceStatus.OK;
+
+  run(() => component.set('address', 'query string'));
+  run(() => component.set('placesService', fakePlacesService));
+
+  fakePlacesService.textSearch.callsArgWith(1, results, status);
+  component.updateLocation = sinon.stub();
+  run(() => component.searchLocation());
+
+  sinon.assert.calledOnce(component.updateLocation);
+  sinon.assert.calledWith(component.updateLocation, results);
+});
+
+test('it sets `lat` & `lng` of the first provided result on `updateLocation`', function(assert) {
+  const results = [{
+    geometry: {
+      location: {
+        lat: () => 12,
+        lng: () => -20
+      }
+    }
+  }, {
+    geometry: {
+      location: {
+        lat: () => 24,
+        lng: () => 100
+      }
+    }
+  }];
+
+  run(() => component.set('attrs', {}));
+  run(() => component.updateLocation(results));
+
+  assert.equal(component.get('lat'), 12);
+  assert.equal(component.get('lng'), -20);
+});
---
+++ tests/unit/components/g-map-route-waypoint-test.js
@@ -0,0 +1,120 @@
+import Ember from 'ember';
+import { moduleForComponent } from 'ember-qunit';
+import test from '../../ember-sinon-qunit/test';
+import GMapComponent from 'ember-g-map/components/g-map';
+import GMapRouteComponent from 'ember-g-map/components/g-map-route';
+import sinon from 'sinon';
+
+const { run } = Ember;
+
+let fakeMarkerObject;
+let routeComponent;
+let component;
+
+moduleForComponent('g-map-route-waypoint', 'Unit | Component | g map route waypoint', {
+  // Specify the other units that are required for this test
+  // needs: ['component:foo', 'helper:bar'],
+  unit: true,
+
+  beforeEach() {
+    const mapComponent = GMapComponent.create();
+    routeComponent = GMapRouteComponent.create({
+      mapContext: mapComponent,
+      registerWaypoint: sinon.stub(),
+      unregisterWaypoint: sinon.stub()
+    });
+    component = this.subject({
+      routeContext: routeComponent
+    });
+  }
+});
+
+test('it triggers `updateWaypoint` on `didInsertElement` event', function() {
+  component.updateWaypoint = sinon.stub();
+  component.trigger('didInsertElement');
+  sinon.assert.calledOnce(component.updateWaypoint);
+});
+
+test('it triggers `unregisterWaypoint` of routeContext on `willDestroyElement` event', function() {
+  component.trigger('willDestroyElement');
+  sinon.assert.calledOnce(routeComponent.unregisterWaypoint);
+});
+
+test('it triggers `updateWaypoint` on `lat` change', function() {
+  component.updateWaypoint = sinon.stub();
+  run(() => component.set('lat', 14));
+  sinon.assert.calledOnce(component.updateWaypoint);
+});
+
+test('it triggers `updateWaypoint` on `lng` change', function() {
+  component.updateWaypoint = sinon.stub();
+  run(() => component.set('lng', 21));
+  sinon.assert.calledOnce(component.updateWaypoint);
+});
+
+test('it triggers `updateWaypoint` only once on `lat` and `lng` change', function() {
+  component.updateWaypoint = sinon.stub();
+  run(() => component.setProperties({ lng: 1, lat: 11 }));
+  sinon.assert.calledOnce(component.updateWaypoint);
+});
+
+test('it sets `waypoint` on `updateWaypoint` with lat/lng present', function(assert) {
+  const point = { 'coords': '10/100' };
+  sinon.stub(google.maps, 'LatLng').returns(point);
+
+  run(() => component.setProperties({
+    lat: 10,
+    lng: 100,
+    stopover: false
+  }));
+  run(() => component.updateWaypoint());
+
+  assert.deepEqual(component.get('waypoint'), {
+    location: point,
+    stopover: false
+  });
+
+  google.maps.LatLng.restore();
+});
+
+test('it doesn\'t change `waypoint` on `updateWaypoint` when no lat present', function(assert) {
+  run(() => component.setProperties({
+    lng: 100,
+    marker: fakeMarkerObject,
+    waypoint: { location: { lat: 1, lng: 2 } }
+  }));
+  run(() => component.updateWaypoint());
+  assert.deepEqual(component.get('waypoint'), { location: { lat: 1, lng: 2 } });
+});
+
+test('it doesn\'t change `waypoint` on `updateWaypoint` when no lng present', function(assert) {
+  run(() => component.setProperties({
+    lat: 100,
+    marker: fakeMarkerObject,
+    waypoint: { location: { lat: 1, lng: 2 } }
+  }));
+  run(() => component.updateWaypoint());
+  assert.deepEqual(component.get('waypoint'), { location: { lat: 1, lng: 2 } });
+});
+
+test('it triggers `updateRoute` on `waypoint` change', function() {
+  component.updateRoute = sinon.stub();
+  run(() => component.set('waypoint', { value: 'new' }));
+  sinon.assert.calledOnce(component.updateRoute);
+});
+
+test('it triggers `registerWaypoint` of routeContext on `updateRoute` if `waypoint` is present', function() {
+  run(() => component.set('waypoint', { some: 'value' }));
+  routeComponent.registerWaypoint = sinon.stub();
+
+  run(() => component.updateRoute());
+  sinon.assert.calledOnce(routeComponent.registerWaypoint);
+});
+
+test('it doesn\'t trigger `registerWaypoint` of routeContext on `updateRoute` without `waypoint`', function() {
+  run(() => component.set('waypoint', undefined));
+  routeComponent.registerWaypoint = sinon.stub();
+
+  run(() => component.updateRoute());
+  sinon.assert.notCalled(routeComponent.registerWaypoint);
+});
