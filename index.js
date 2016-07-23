ee40fee88c55875903064b6cfaa53c7a572612e5
Steve Szczecina <steveszc@gmail.com>
2016-07-22 21:37:36 -0600

    Add config for exclude, client ID, version
    - 'exclude' option to prevent addon from inserting the google maps API script tag
    - 'version' option to specify a version of the google maps API
    - 'client' option to use a Client ID instead of a key (for Google Maps API Premium Plan users)

--- index.js
+++ index.js
@@ -21,6 +21,19 @@ module.exports = {
       if (key) {
         params.push('key=' + encodeURIComponent(key));
       }
+      
+      var version = gMapConfig.version;
+      if (version) {
+        params.push('v=' + encodeURIComponent(version));
+      }
+      
+      var client = gMapConfig.client;
+      if (client) {
+        params.push('client=' + encodeURIComponent(client));
+        if (!version) {
+          params.push('v=3');
+        }
+      }
 
       var libraries = gMapConfig.libraries;
       if (libraries && libraries.length) {
@@ -39,6 +52,11 @@ module.exports = {
 
       src += '?' + params.join('&');
       content = '<script type="text/javascript" src="' + src + '"></script>';
+      
+      var exclude = gMapConfig.exclude;
+      if (exclude) {
+        content = ''
+      }
     }
 
     return content;
